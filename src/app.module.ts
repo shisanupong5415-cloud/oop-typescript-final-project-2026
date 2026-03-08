1a298341f909235c1349e51f2c0735ba1eb9f393
kuuhakumu <play.immin00@gmail.com>
2026-03-08 18:01:36 +0900

    remove duplicate WebController registration in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TicketsModule } from './modules/tickets/tickets.module';
 import { CustomerModule } from './modules/customer/customer.module';
 import { WebModule } from './modules/web/web.module';
-import { WebController } from './modules/web/web.controller';
 import { AppController } from './app.controller';
 
 @Module({
@@ -11,6 +10,6 @@ import { AppController } from './app.controller';
     CustomerModule,
     WebModule,
   ],
-  controllers: [WebController,AppController],
+  controllers: [AppController],
 })
 export class AppModule {}
